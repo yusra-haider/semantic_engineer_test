1ff5a012e759e1e4d81374fa7be86b73fff8a7f7
Yusra Haider <yusra.haider@gmail.com>
2022-07-12 15:54:29 +0100

    getting cli.py to work

--- pheno/cli.py
+++ pheno/cli.py
@@ -4,3 +4,55 @@ Please develop a Command Line Interface (CLI) that
    - executes the `compute_phenotype_similarity()` method defined in pheno.py
    - writes the resulting pandas data frame out as a TSV file.
 """
+
+import argparse
+from pheno.pheno import parse_input_file
+
+
+def main():
+    """
+    Parse user-provided command-line arguments.
+    """
+    parser = argparse.ArgumentParser()
+    # todo: double check the required attr
+    parser.add_argument('--input_file', type=str, help='path to a text file with a list of disease identifiers')
+
+    args = parser.parse_args()
+
+    if args.input_file:
+        parse_input_file(input_file_name=args.input_file)
+
+    else:
+        raise Exception("");
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+# pheno.py
+# Import the argparse library
+# import argparse
+#
+# import os
+# import sys
+# import pheno
+#
+# # Create the parser
+# my_parser = argparse.ArgumentParser(description='takes as an input a text file with a list of disease identifiers')
+#
+# # Add the arguments
+#
+# my_parser.add_argument('name')
+#
+# # Execute the parse_args() method
+# args = my_parser.parse_args()
+#
+# filename = args.name
+#
+# df = compute_phenotype_similarity(filename)
+#
+# df.to_csv('example.tsv', sep="\t")
--- pheno/pheno.py
+++ pheno/pheno.py
@@ -12,5 +12,60 @@ import pandas as pd
 from typing import List
 
 
-def compute_phenotype_similarity(diseases: List[str]) -> pd.DataFrame:
-    pass
+def parse_input_file(input_file_name) -> []:
+    # todo: parse the input file to get list of diseases
+    # returning dummy list for now
+    print(input_file_name)
+    return [1,2,3]
+
+
+def compute_phenotype_similarity(name):
+    handle = open(name)
+    df = pd.DataFrame([get_list_element(disease) for disease in handle]).T
+    handle = open(name)
+    df.columns = [disease.rstrip() for disease in handle]
+    df_intersection = get_intersection(df)
+    return df_intersection
+
+
+def get_associations(line):
+    # compute_from_line(line)
+    servernew = 'https://api.monarchinitiative.org/api/bioentity/disease/'
+    id = line.rstrip()
+    teststring = servernew + id + '/phenotypes'
+    r = requests.get(teststring)
+    decode = r.json()
+    associations = decode['associations']
+    return id, associations
+
+
+def give_element(id, associations):
+    return numpy.unique([elements['object']['label'] for elements in associations])
+
+
+def get_list_element(line):
+    id, associations = get_associations(line)
+    return give_element(id, associations)
+
+
+def get_intersection(df):
+    df_out = pd.DataFrame()
+    # sum_AuB = []
+
+    for i, col in enumerate(df.columns):
+        # making a list of all column which has to be compared with col
+        other_col = [x for x in df.columns[i + 1:]]
+        for oCol in other_col:
+            # using a set we can find a intersection between 2 list and count them
+            Intersection = len(set(df[col].values.tolist()).intersection(df[oCol].values.tolist()))
+            sum_A = len(set(df[col].tolist()))
+            sum_B = len(set(df[oCol].tolist()))
+            sum_AuB = sum_A + sum_B - Intersection
+            Jaccard = Intersection / sum_AuB
+            # storing all Intersections with their respective column in separate df
+            df_out = df_out.append([(col, oCol, Jaccard)], sort=True, ignore_index=True)
+
+    df_out.columns = ['disease_1', 'disease_2', 'Jaccard']
+    df_out.sort_values(by=['Jaccard'], ascending=[False], inplace=True)
+
+    return df_out
